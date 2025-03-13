e8a3df0336679d84e17f46e333b045cd85c8aa79
Shayan <shayanramezani98@gmail.com>
2025-03-13 12:19:07 +0100

    fix bug in neuralnet

--- src/models/neural_net.py
+++ src/models/neural_net.py
@@ -104,7 +104,10 @@ class NeuralNetWrapper(ClassifierMixin, BaseEstimator):
             for (X_batch,) in dataloader:
                 logits = self.model(X_batch).squeeze()
                 probs = sigmoid(logits)
-                all_probs.extend(probs.cpu().numpy().tolist() if probs.shape else [probs.cpu().numpy().item()])
+                if probs.shape:
+                    all_probs.extend(probs.cpu().numpy().tolist())
+                else:
+                    all_probs.append(probs.cpu().numpy().item())
 
         # Format as scikit-learn compatible 2D array with columns [prob_class_0, prob_class_1]
         all_probs = np.array(all_probs)
